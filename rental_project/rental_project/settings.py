5eda3973b3fd7bf79ecce98cfc4125f4de868a21
Viktoria599 <260583sla@mail.ru>
2026-06-09 15:54:53 +0300

    Update settings.py

--- rental_project/rental_project/settings.py
+++ rental_project/rental_project/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'rental_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
